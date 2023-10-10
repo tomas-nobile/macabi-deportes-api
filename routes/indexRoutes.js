b207583e31be41b62aab19de8dc098951eeeeda3
Gino Basile <basilegino@hotmail.com>
2023-10-10 19:12:45 -0300

    deportes routes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,11 +3,7 @@ import { Router } from "express";
 import usuarioRoutes from "./usuario.Routes.js";
 import fechaRoutes from "./Fecha.Routes.js";
 import asistenciaRoutes from "./Asistencia.Routes.js";
-<<<<<<< HEAD
 import deporteRoutes from "./deporteRoutes.js"
-=======
-import socioRoutes from "./socio.Routes.js";
->>>>>>> 326df67baf1452715142376621b295c516078389
 
 const indexRoutes=Router()
 
@@ -15,12 +11,7 @@ const indexRoutes=Router()
 indexRoutes.use("/usuario", usuarioRoutes);
 indexRoutes.use("/asistencia", asistenciaRoutes);
 indexRoutes.use("/fecha", fechaRoutes);
-<<<<<<< HEAD
 indexRoutes.use("/deporte", deporteRoutes);
-=======
-indexRoutes.use("/socio", socioRoutes);
-
->>>>>>> 326df67baf1452715142376621b295c516078389
 
 
 export default indexRoutes
