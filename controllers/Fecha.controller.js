3f41301f823ad35fa3a3a0d1ab3a2aafc372e590
MatiasAltmann <102705202+MatiasAltmann@users.noreply.github.com>
2023-10-20 21:06:32 -0300

    arregloValidacionesCreacionDeFechas

--- controllers/Fecha.controller.js
+++ controllers/Fecha.controller.js
@@ -1,16 +1,19 @@
 import { Fecha } from "../models/index.js";
 import AsistenciaController from "./Asistencia.controller.js";
+import CategoriaController from "./CategoriaController.js";
 import SociosXCategoriasController from "./SociosXCategoriasController.js";
 class FechaController {
 
     constructor() { }
 
     createFecha = async (req, res, next) => {
+        const {idCategoria, fechaCalendario ,tipo, idSocios} = req.body;
+        console.log("La fecha es : " + fechaCalendario + " el id categoria: " + idCategoria + " y el tipo: " + tipo);
+
+
         try {
-            const {idCategoria, fechaCalendario ,tipo, idSocios} = req.body;
-          if(await this.existeFecha(idCategoria,fechaCalendario))  {
-            throw new Error("La fecha no puede ser creada porque ya existe esta fecha en esta categoria");
-          }
+          await  this.valiadorDeParametrosCreate(idCategoria,fechaCalendario,tipo,idSocios);
+          console.log("id Socios:...idSocios");
             const result = await Fecha.create({ idCategoria, fechaCalendario , tipo });
 
             if (!result) throw new Error("La fecha no puede ser creada");
@@ -35,7 +38,9 @@ class FechaController {
                 }else {
                     try {
                         idSocios.forEach(socio => {
-                            asistenciaController.crearAsistenciaAuxiliar(result.dataValues.idFecha,socio.idSocio,null)
+                           
+                                asistenciaController.crearAsistenciaAuxiliar(result.dataValues.idFecha,socio.idSocio,null)
+                            
                         });
                     }catch(e){
                         console.log("Hubo un error con las citaciones:", error);
@@ -50,7 +55,7 @@ class FechaController {
                 .status(200)
                 .send({ success: true, message: "Fecha creada con exito" });
         } catch (error) {
-            res.status(400).send({ success: false, message: error.message });
+            res.status(400).send({ success: false, message: error.message}); //Le pongo este dato para q se valide
         }
     };
 
@@ -74,6 +79,55 @@ class FechaController {
     }
 
  }
+
+ async valiadorDeParametrosCreate(idCategoria, fechaCalendario ,tipo, idSocios){
+
+    if(idCategoria == null || fechaCalendario == null || tipo == null ){
+        throw new Error("No se puede crear una fecha sin todos los datos necesarios (Categoria, fecha y tipo)");
+    }else {
+        if(tipo == "C") {
+            if(idSocios == null) {
+                throw new Error("Error. No se respeta el formato de envio de socios"); //Chequear estas validaciones
+            }
+            console.log("Llegue a validar aca");
+            if(idSocios.length == 0 ) {
+                console.log("Es 0");
+                console.log(idSocios.length);
+                throw new Error("Error. No se ingresaron socios para cargar");
+        
+            }else {
+                console.log("Es más de 0");
+                console.log(idSocios.length);
+                this.validarIdSocioCompleto(idSocios)
+            }
+        }
+        
+    }
+    
+    let categoriaController = new CategoriaController();
+    if(!await categoriaController.existeCategoria(idCategoria)) {
+        throw new Error("No existe la categoria");
+    }
+    
+    if(await this.existeFecha(idCategoria,fechaCalendario))  {
+    throw new Error("La fecha no puede ser creada porque ya existe esta fecha en esta categoria.");
+    }
+ 
+
+    //No valido el tipo. ya q te da error auotmatico (Si le da la fecha mal aunque te da mal el tipo , primero te corrige eso, pero no se rompe.)
+    
+
+ }
+
+  validarIdSocioCompleto(sociosList){
+    for (const socio of sociosList) {
+        if(!socio.idSocio){
+            throw new Error("Error en el formato que se enviaron los socios")
+        }
+    }
+  }
+
+ 
 
 }
 
