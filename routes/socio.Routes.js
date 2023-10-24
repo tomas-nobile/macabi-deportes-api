0c3fab81607122ab651a7c5fdfebe8b14bb64008
MatiasAltmann <102705202+MatiasAltmann@users.noreply.github.com>
2023-10-23 22:23:30 -0300

    commit merge

--- routes/socio.Routes.js
+++ routes/socio.Routes.js
@@ -5,13 +5,9 @@ const socioController = new SocioController();
 
 const socioRoutes = Router();
 
-<<<<<<< HEAD
-socioRoutes.post("/", socioController.createSocio)
-=======
 socioRoutes.post("/", socioController.crearSocio);
 socioRoutes.get("/getSocios", socioController.traerTodosLosSocios);
 socioRoutes.get("/:idSocio", socioController.traerSocioPorId);
 socioRoutes.put("/:idSocio", socioController.updateSocio);
->>>>>>> origin/JuampiSrint2
 
 export default socioRoutes
