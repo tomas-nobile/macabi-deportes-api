8b67792699e862b03c010b1f8135403f20e73946
MatiasAltmann <102705202+MatiasAltmann@users.noreply.github.com>
2023-10-27 00:26:24 -0300

    testing

--- routes/usuario.Routes.js
+++ routes/usuario.Routes.js
@@ -9,13 +9,12 @@ const usuarioRoutes = Router();
 // Rutas
 
 usuarioRoutes.post("/login", userController.logIn);
-usuarioRoutes.get("/:idUsuario", userController.getUserById);
 
-//con necesidad de logueo
-//usuarioRoutes.use(validateAccess);
+usuarioRoutes.use(validateAccess);
 
 usuarioRoutes.get("/me", userController.me);
 usuarioRoutes.get("/getAll", userController.getAllUsers);
+usuarioRoutes.get("/:idUsuario", userController.getUserById);
 usuarioRoutes.get("/:idUsuario/deportes", userController.getDeportesPorCoordinador);
 usuarioRoutes.get("/:idUsuario/categorias", userController.getCategoriasPorProfesor);
 usuarioRoutes.get("/profesores", userController.getUserProfesores)
@@ -28,4 +27,11 @@ usuarioRoutes.patch("/:idUsuario", userController.patchUserById);
 usuarioRoutes.delete("/:idUsuario", userController.deleteUserById);  
 
 
+
+
 export default usuarioRoutes;
+
+
+
+
+
