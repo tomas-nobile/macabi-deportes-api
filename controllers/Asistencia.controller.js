0d999c445ed658f4e2054d6f7dff3e08269f77f9
Gino Basile <basilegino@hotmail.com>
2023-10-31 18:30:00 -0300

    Agrego tomar y modificar asistencia

--- controllers/Asistencia.controller.js
+++ controllers/Asistencia.controller.js
@@ -1,79 +1,111 @@
 import { Asistencia, Socio } from "../models/index.js";
 
 class AsistenciaController {
+  constructor() {}
 
-    constructor() { }
+  createAsistencia = async (req, res, next) => {
+    try {
+      const { idFecha, idSocio, estado } = req.body;
 
-    createAsistencia = async (req, res, next) => {
-        try {
+      const result = await Asistencia.create({ idFecha, idSocio, estado });
+      if (!result) throw new Error("La asistencia no puede ser creada");
+      res
+        .status(200)
+        .send({ success: true, message: "Asistencia creada con exito" });
+    } catch (error) {
+      res.status(400).send({ success: false, message: error.message });
+    }
+  };
 
-            const { idFecha ,idSocio, estado} = req.body;
+  crearAsistenciaAuxiliar = async (idFecha, idSocio, estado) => {
+    try {
+      const result = await Asistencia.create({ idFecha, idSocio, estado });
+      if (!result) throw new Error("La asistencia no puede ser creada");
+    } catch (e) {
+      throw e;
+    }
+  };
 
-            const result = await Asistencia.create({idFecha ,idSocio, estado});
-            if (!result) throw new Error("La asistencia no puede ser creada");
-            res
-                .status(200)
-                .send({ success: true, message: "Asistencia creada con exito" });
-        } catch (error) {
-            res.status(400).send({ success: false, message: error.message });
-        }
-    };
-
-    
-
-    crearAsistenciaAuxiliar = async (idFecha,idSocio,estado) => {
-        try {
-
-            const result = await Asistencia.create({idFecha ,idSocio, estado});
-            if (!result) throw new Error("La asistencia no puede ser creada");
-
-        }catch(e){
-            throw e;
-        }
-        
-    
-}
-
-getAsistenciaFecha = async (req, res) => {
+  getAsistenciaFecha = async (req, res) => {
     const { idFecha } = req.params;
     try {
       const asistencias = await Asistencia.findAll({
         where: { idFecha },
-        attributes: ['idSocio', 'estado'],
+        attributes: ["idSocio", "estado"],
       });
-  
+
       if (asistencias.length === 0) {
-        res.status(200).send({ success: true, message: 'No hay socios cargados en esta fecha', result: [] });
+        res
+          .status(200)
+          .send({
+            success: true,
+            message: "No hay socios cargados en esta fecha",
+            result: [],
+          });
       } else {
         // Obtener los idSocio de las asistencias
-        const idSocios = asistencias.map(asistencia => asistencia.idSocio);
-  
+        const idSocios = asistencias.map((asistencia) => asistencia.idSocio);
+
         // Consultar los nombres y apellidos de los socios basado en los idSocio
         const socios = await Socio.findAll({
           where: { idSocio: idSocios }, // Filtrar por los idSocio
-          attributes: ['idSocio', 'nombre', 'apellido'], // Seleccionar los campos necesarios
+          attributes: ["idSocio", "nombre", "apellido"], // Seleccionar los campos necesarios
         });
-  
+
         // Mapear los nombres y apellidos de los socios a las asistencias
-        const asistenciasConNombres = asistencias.map(asistencia => {
-          const socio = socios.find(s => s.idSocio === asistencia.idSocio);
+        const asistenciasConNombres = asistencias.map((asistencia) => {
+          const socio = socios.find((s) => s.idSocio === asistencia.idSocio);
           return {
             idSocio: asistencia.idSocio,
-            nombreSocio: socio ? socio.nombre : 'Socio no encontrado',
-            apellidoSocio: socio ? socio.apellido : 'Socio no encontrado',
+            nombreSocio: socio ? socio.nombre : "Socio no encontrado",
+            apellidoSocio: socio ? socio.apellido : "Socio no encontrado",
             estado: asistencia.estado,
           };
         });
-  
-        res.status(200).send({ success: true, message: 'Asistencias encontradas:', result: asistenciasConNombres });
+
+        res
+          .status(200)
+          .send({
+            success: true,
+            message: "Asistencias encontradas:",
+            result: asistenciasConNombres,
+          });
       }
     } catch (error) {
-      console.error('Error al obtener las asistencias:', error);
-      res.status(500).json({ error: 'Error al obtener las asistencias' });
+      console.error("Error al obtener las asistencias:", error);
+      res.status(500).json({ error: "Error al obtener las asistencias" });
     }
   };
 
-    
+  modificarAsistencia = async (req, res) => {
+    const { idFecha } = req.params;
+    const cambiosAsistencia = req.body;
+    try {
+      console.log("Datos recibidos:", cambiosAsistencia);
+
+      for (const cambio of cambiosAsistencia) {
+        const { idSocio, estado } = cambio;
+
+        let asistencia = await Asistencia.findOne({
+          where: { idFecha, idSocio },
+        });
+
+        if (!asistencia) {
+          asistencia = await Asistencia.create({ idFecha, idSocio, estado });
+        } else {
+          asistencia.estado = estado;
+          await asistencia.save();
+        }
+      }
+
+      res
+        .status(200)
+        .send({ success: true, message: "Asistencias actualizadas con éxito" });
+    } catch (error) {
+      console.error("Error en el controlador:", error);
+      res.status(500).send({ success: false, message: "Error en el servidor" });
+    }
+  };
 }
 
-export default AsistenciaController
+export default AsistenciaController;
